4600170594b00f6d7e2ff733162ffe125c404f9f
Merrick Sapsford <merrick@sapsford.tech>
2017-07-07 14:38:20 +0100

    Add ability to cancel requests

--- HearthisAt-Player/Service/Requests/ExecutableRequest.swift
+++ HearthisAt-Player/Service/Requests/ExecutableRequest.swift
@@ -7,18 +7,12 @@
 //
 
 import Foundation
-
-protocol ExecutableRequestDelegate: class {
-    
-    func request(doesRequireCancellation request: Request)
-}
+import Alamofire
 
 class ExecutableRequest: Request {
     
     // MARK: Properties
-    
-    weak var delegate: ExecutableRequestDelegate?
-    
+        
     private var _response: Response?
     override var response: Response? {
         set {
@@ -32,17 +26,16 @@ class ExecutableRequest: Request {
     override var inProgress: Bool {
         set {
             _inProgress = inProgress
-            if inProgress == false {
-                self.delegate = nil
-            }
         } get {
             return _inProgress
         }
     }
     
+    weak var dataRequest: DataRequest?
+    
     // MARK: Actions
     
     override func cancel() {
-        delegate?.request(doesRequireCancellation: self)
+        dataRequest?.cancel()
     }
 }
