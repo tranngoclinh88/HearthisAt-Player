e6ffd296d1d5a2d92ab422f57ac917634d92c306
Merrick Sapsford <merrick@sapsford.tech>
2017-07-07 11:26:05 +0100

    Add objects to user profile model

--- HearthisAt-Player/Service/Controllers/Artist/ArtistApiController.swift
+++ HearthisAt-Player/Service/Controllers/Artist/ArtistApiController.swift
@@ -13,6 +13,37 @@ class ArtistApiController: ApiController, ArtistController {
     func loadProfile(for artist: Artist,
                      success: ((ArtistProfile) -> Void)?,
                      failure: Controller.MethodFailure?) {
+        guard let username = artist.permalink else {
+            failure?(ControllerError.unknownMethodFailure)
+            return
+        }
         
+        guard let request = requestBuilder.build(for: .artist(username: username),
+                                                 method: .get) else {
+                                                    failure?(ControllerError.unknownMethodFailure)
+                                                    return
+        }
+        
+        // perform the request
+        requestExecutor.execute(request,
+                                success:
+            { (request, response, data) in
+                guard let data = data else {
+                    failure?(ControllerError.unexpectedResponse)
+                    return
+                }
+                do {
+                    
+                    let json = try JSONSerialization.jsonObject(with: data, options: .mutableContainers) as! [String : Any]
+                    let profile = ArtistProfile(JSON: json)
+
+//                    success?(feed, trSacks)
+                    
+                } catch {
+                    failure?(ControllerError.unexpectedResponse)
+                }
+        }) { (request, response, error) in
+            failure?(error)
+        }
     }
 }
--- HearthisAt-Player/Service/Model/UserProfile.swift
+++ HearthisAt-Player/Service/Model/UserProfile.swift
@@ -11,6 +11,47 @@ import ObjectMapper
 
 typealias ArtistProfile = UserProfile
 
-struct UserProfile {
+struct UserProfile: Mappable {
     
+    private(set) var id: Int?
+    private(set) var username: String?
+    private(set) var permalink: String?
+    private(set) var uri: String?
+    private(set) var description: String?
+    private(set) var geo: String?
+    
+    private(set) var permalinkUrl: URL?
+    private(set) var thumbnailUrl: URL?
+    private(set) var avatarUrl: URL?
+    private(set) var backgroundUrl: URL?
+    
+    private(set) var trackCount: Int?
+    private(set) var playlistCount: Int?
+    private(set) var likesCount: Int?
+    private(set) var followersCount: Int?
+    private(set) var followingCount: Int?
+    
+    // MARK: Mappable
+    
+    init?(map: Map) {
+        
+    }
+    
+    mutating func mapping(map: Map) {
+        self.id             <- (map["id"], IntTransform())
+        self.permalink      <- map["permalink"]
+        self.username       <- map["username"]
+        self.uri            <- map["uri"]
+        self.permalinkUrl   <- (map["permalink_url"], URLTransform())
+        self.thumbnailUrl   <- (map["thumb_url"], URLTransform())
+        self.avatarUrl      <- (map["avatar_url"], URLTransform())
+        self.backgroundUrl  <- (map["background_url"], URLTransform())
+        self.description    <- map["description"]
+        self.geo            <- map["geo"]
+        self.trackCount     <- (map["track_count"], IntTransform())
+        self.playlistCount  <- (map["playlist_count"], IntTransform())
+        self.likesCount     <- (map["likes_count"], IntTransform())
+        self.followersCount <- (map["followers_count"], IntTransform())
+        self.followingCount <- (map["following_count"], IntTransform())
+    }
 }
