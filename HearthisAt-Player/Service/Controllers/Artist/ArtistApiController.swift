bd053323dca1ae2c2c9d2229069402ac82dcfd02
Merrick Sapsford <merrick@sapsford.tech>
2017-07-07 14:47:46 +0100

    Add artist profile mapping

--- HearthisAt-Player/Service/Controllers/Artist/ArtistApiController.swift
+++ HearthisAt-Player/Service/Controllers/Artist/ArtistApiController.swift
@@ -10,6 +10,12 @@ import Foundation
 
 class ArtistApiController: ApiController, ArtistController {
     
+    // MARK: Properties
+    
+    private var artistProfileMap: [String : ArtistProfile] = [:]
+    
+    // MARK: Methods
+    
     func loadProfile(for artist: Artist,
                      success: ((ArtistProfile) -> Void)?,
                      failure: Controller.MethodFailure?) {
@@ -35,9 +41,13 @@ class ArtistApiController: ApiController, ArtistController {
                 do {
                     
                     let json = try JSONSerialization.jsonObject(with: data, options: .mutableContainers) as! [String : Any]
-                    let profile = ArtistProfile(JSON: json)
-
-//                    success?(feed, trSacks)
+                    guard let profile = ArtistProfile(JSON: json) else {
+                        failure?(ControllerError.unexpectedResponse)
+                        return
+                    }
+                    
+                    self.artistProfileMap[username] = profile
+                    success?(profile)
                     
                 } catch {
                     failure?(ControllerError.unexpectedResponse)
@@ -45,5 +55,10 @@ class ArtistApiController: ApiController, ArtistController {
         }) { (request, response, error) in
             failure?(error)
         }
+    }
+    
+    func profile(for artist: Artist) -> ArtistProfile? {
+        guard let identifier = artist.permalink else { return nil }
+        return artistProfileMap[identifier]
     }
 }
--- HearthisAt-Player/Service/Controllers/Artist/ArtistController.swift
+++ HearthisAt-Player/Service/Controllers/Artist/ArtistController.swift
@@ -10,6 +10,18 @@ import Foundation
 
 protocol ArtistController: Controller {
     
+    /// Get the profile for an artist.
+    ///
+    /// - Parameter artist: The artist.
+    /// - Returns: The artist profile if loaded.
+    func profile(for artist: Artist) -> ArtistProfile?
+    
+    /// Load a profile for an artist.
+    ///
+    /// - Parameters:
+    ///   - artist: The artist.
+    ///   - success: Execution on successful method.
+    ///   - failure: Execution on failed method.
     func loadProfile(for artist: Artist,
                      success: ((ArtistProfile) -> Void)?,
                      failure: MethodFailure?)
