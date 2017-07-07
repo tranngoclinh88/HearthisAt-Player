cea2a4734c798c704df7f1a1973619e4c142b283
Merrick Sapsford <merrick@sapsford.tech>
2017-07-07 18:24:50 +0100

    Add back button to tracks header

---
+++ HearthisAt-Player/Resource/FontPalette.swift
@@ -0,0 +1,20 @@
+//
+//  FontPalette.swift
+//  HearthisAt-Player
+//
+//  Created by Merrick Sapsford on 07/07/2017.
+//  Copyright © 2017 Merrick Sapsford. All rights reserved.
+//
+
+import UIKit
+
+extension UIFont {
+    
+    static var hta_title: UIFont {
+        return UIFont.systemFont(ofSize: 20.0, weight: UIFontWeightMedium)
+    }
+    
+    static var hta_subtitle: UIFont {
+        return UIFont.systemFont(ofSize: 17.0, weight: UIFontWeightRegular)
+    }
+}
