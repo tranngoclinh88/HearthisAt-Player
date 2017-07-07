13464c0de88cf8767d2c52809ba327d8a4148afb
Merrick Sapsford <merrick@sapsford.tech>
2017-07-07 16:27:21 +0100

    Integrate profile provider into list

--- HearthisAt-Player/UI/Screens/Feed/FeedArtistProfileProvider.swift
+++ HearthisAt-Player/UI/Screens/Feed/FeedArtistProfileProvider.swift
@@ -8,15 +8,63 @@
 
 import Foundation
 
+protocol FeedArtistProfileProviderDelegate: class {
+    
+    func feedArtistProfileProvider(_ provider: FeedArtistProfileProvider,
+                                   didUpdate state: FeedArtistProfileProvider.State)
+}
+
 class FeedArtistProfileProvider {
+    
+    // MARK: Types
+    
+    enum State {
+        case ready
+        case loading
+        case available(profile: ArtistProfile)
+        case failed(error: Error)
+    }
     
     // MARK: Properties
     
     let artist: Artist
-        
+    let service: Service
+    
+    weak var delegate: FeedArtistProfileProviderDelegate?
+    
+    private(set) var currentState: State = .ready {
+        didSet {
+            delegate?.feedArtistProfileProvider(self, didUpdate: currentState)
+        }
+    }
+    
     // MARK: Init
     
-    init(for artist: Artist) {
+    init(for artist: Artist, service: Service) {
         self.artist = artist
+        self.service = service
+        
+        reloadData()
+    }
+    
+    func reloadData(forceLoad: Bool = false) {
+        if let profile = service.artistController.profile(for: artist), forceLoad == false {
+            self.currentState = .available(profile: profile)
+        } else { // perform load
+            currentState = .loading
+            service.artistController.loadProfile(for: artist,
+                                                 success:
+                { (profile) in
+                    self.currentState = .available(profile: profile)
+            }) { (error) in
+                switch self.currentState {
+                    
+                case .available(_):() // do nothing if already available
+                    
+                default:
+                    self.currentState = .failed(error: error)
+                }
+            }
+        }
     }
 }
--- HearthisAt-Player/UI/Screens/Feed/Views/FeedItemCell.swift
+++ HearthisAt-Player/UI/Screens/Feed/Views/FeedItemCell.swift
@@ -51,6 +51,18 @@ class FeedItemCell: UITableViewCell {
             // TODO - Image loading
         }
     }
+    weak var profileProvider: FeedArtistProfileProvider? {
+        didSet {
+            if oldValue?.delegate === self {
+                oldValue?.delegate = nil
+            }
+            profileProvider?.delegate = self
+            
+            if let currentState = profileProvider?.currentState {
+                updateForProfileProvider(with: currentState)
+            }
+        }
+    }
     
     // MARK: Lifecycle
     
@@ -58,5 +70,51 @@ class FeedItemCell: UITableViewCell {
         super.awakeFromNib()
         
         backgroundColor = .clear
+    }
+    
+    override func prepareForReuse() {
+        super.prepareForReuse()
+        
+        avatarImageView.image = nil
+        title = nil
+        subtitle = nil
+    }
+    
+    // MARK: Profile Provider
+    
+    fileprivate func updateForProfileProvider(with state: FeedArtistProfileProvider.State) {
+        switch state {
+            
+        case .loading:
+            subtitle = "screen.feed.cell.profiledetails.loading".localized()
+            
+        case .available(let profile):
+            subtitle = generateProfileDetails(for: profile)
+            
+        case .failed(_):
+            subtitle = "screen.feed.cell.profiledetails.unavailable".localized()
+            
+        default:
+            subtitle = ""
+        }
+    }
+    
+    private func generateProfileDetails(for profile: ArtistProfile) -> String {
+        var detailsString = ""
+        if let permalink = profile.permalink {
+            detailsString.append("\(permalink)")
+        }
+        if let trackCount = profile.trackCount {
+            detailsString.append(" // \(trackCount) Tracks")
+        }
+        return detailsString
+    }
+}
+
+extension FeedItemCell: FeedArtistProfileProviderDelegate {
+    
+    func feedArtistProfileProvider(_ provider: FeedArtistProfileProvider,
+                                   didUpdate state: FeedArtistProfileProvider.State) {
+        updateForProfileProvider(with: state)
     }
 }
