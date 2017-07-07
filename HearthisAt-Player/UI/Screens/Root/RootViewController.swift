c3fdbf50d1b938b114add0549ec69f094f65696e
Merrick Sapsford <merrick@sapsford.tech>
2017-07-07 22:58:00 +0100

    Add pause / play button functionality

--- HearthisAt-Player/UI/Screens/Root/RootViewController.swift
+++ HearthisAt-Player/UI/Screens/Root/RootViewController.swift
@@ -27,6 +27,7 @@ class RootViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         
+        miniPlayer.delegate = self
         service.playbackController.notificationService.add(listener: self)
         
         backgroundGradientView.colors = UIColor.hta_backgroundGradient
@@ -39,12 +40,16 @@ extension RootViewController: PlaybackControllerNotifyable {
                             didUpdate state: PlayableItem.State,
                             of item: PlayableItem) {
         
-        updateBackgroundView(for: state, item: item)
+        updateBackgroundView(for: item, state: state)
+        updateMiniPlayer(for: item, state: state)
     }
     
-    private func updateBackgroundView(for state: PlayableItem.State,
-                                      item: PlayableItem) {
-        guard let artworkUrl = item.object.playableArtworkUrl else { return }
+    private func updateBackgroundView(for item: PlayableItem,
+                                      state: PlayableItem.State) {
+        guard let artworkUrl = item.object.playableArtworkUrl else {
+            backgroundAlbumArtView.imageUrl = nil
+            return
+        }
         
         switch state {
         case .playing, .paused, .loading, .ready:
@@ -52,6 +57,52 @@ extension RootViewController: PlaybackControllerNotifyable {
             
         default:
             backgroundAlbumArtView.imageUrl = nil
+        }
+    }
+    
+    private func updateMiniPlayer(for item: PlayableItem,
+                                  state: PlayableItem.State) {
+        miniPlayer.buttonState = miniPlayerButtonState(for: state)
+        
+        switch state {
+        case .ready:
+            miniPlayer.title = nil
+            miniPlayer.subtitle = nil
+            
+        case .playing, .paused:
+            miniPlayer.title = item.object.playableTitle ?? "general.title.unknown".localized()
+            miniPlayer.subtitle = item.object.playableDetails ?? "general.title.unknown".localized()
+            
+        case .loading:
+            miniPlayer.title = item.object.playableTitle ?? "general.title.unknown".localized()
+            miniPlayer.subtitle = "general.title.loading".localized()
+            
+        case .failed:
+            miniPlayer.subtitle = "general.title.failed".localized()
+        }
+        
+    }
+    
+    private func miniPlayerButtonState(for itemState: PlayableItem.State) -> MiniPlayer.ButtonState {
+        switch itemState {
+        case .failed, .paused, .ready:
+            return .play
+        default:
+            return .pause
+        }
+    }
+}
+
+extension RootViewController: MiniPlayerDelegate {
+    
+    func miniPlayer(playPauseButtonPressed miniPlayer: MiniPlayer) {
+        guard let currentItem = service.playbackController.currentItem else { return }
+        
+        switch miniPlayer.buttonState {
+        case .pause:
+            currentItem.pause()
+        default:
+            currentItem.play()
         }
     }
 }
